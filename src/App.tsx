1c73256a271277f2b71c7675312419d7031e6591
Tony Chow <tonychow.0929@icloud.com>
2024-01-01 17:59:46 +0800

    Make h1 navigatable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Link, Routes, Route } from "react-router-dom";
 
 import TutorialList from "./TutorialList";
 import TutorialPage from "./TutorialPage";
@@ -10,9 +10,11 @@ import styles from "./App.module.scss";
 function App() {
   return (
     <div className={styles.app}>
-      <h1 className={styles.title}>Monotone</h1>
-      <h2 className={styles.description}>Miscellaneous tutorials</h2>
       <HashRouter>
+        <Link className={styles.titleLink} to="/">
+          <h1 className={styles.title}>Monotone</h1>
+        </Link>
+        <h2 className={styles.description}>Miscellaneous tutorials</h2>
         <Routes>
           <Route path="/" element={<TutorialList />} />
           <Route path="/tutorial/:id" element={<TutorialPage />} />
